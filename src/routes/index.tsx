200d80e7abb201d6465b8822e71badb6387ae774
vinnichase <vincenz.koop@gmail.com>
2024-11-20 20:56:27 +0100

    added logo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,16 +7,19 @@ export default function Home() {
             <Title>E-Rechner</Title>
             <div class="flex flex-col items-center">
                 <div class="container mb-12 max-w-[1000px]">
-                    <div class="mt-24 flex flex-col gap-4">
-                        <h1 class="text-6xl">E-Rechner</h1>
-                        <p class="text-2xl italic">
-                            Das Standard-Tool für Deutschland zur Erstellung offizieller E-Rechnungen.
-                        </p>
-                        <p class="text-lg">
-                            E-Rechner ist ein einfaches Werkzeug zur Erstellung rechtssicherer E-Rechnungen. Die
-                            Verarbeitung ist kostenlos und erfolgt vollständig lokal auf Ihrem Computer – keine
-                            Datenübertragung, keine Speicherung auf externen Servern.
-                        </p>
+                    <div class="mt-24 flex flex-row items-start gap-12">
+                        <div class="flex flex-col gap-4">
+                            <h1 class="text-6xl">E-Rechner</h1>
+                            <p class="text-2xl italic">
+                                Das Standard-Tool für Deutschland zur Erstellung offizieller E-Rechnungen.
+                            </p>
+                            <p class="text-lg">
+                                E-Rechner ist ein einfaches Werkzeug zur Erstellung rechtssicherer E-Rechnungen. Die
+                                Verarbeitung ist kostenlos und erfolgt vollständig lokal auf Ihrem Computer – keine
+                                Datenübertragung, keine Speicherung auf externen Servern.
+                            </p>
+                        </div>
+                        <img src="Logo.svg" width={300} />
                     </div>
                     <XRechnung />
                 </div>
